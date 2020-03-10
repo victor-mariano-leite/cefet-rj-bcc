6bdedbd7c9ef53039e8200efec0970e3bad5a0c8
victor-mariano-leite <marianovictor44@gmail.com>
2020-03-10 01:55:05 -0300

    corrigindo argumento do serach de FileSearch

--- materias/obrigatorias/organizacao-estrutura-de-arquivos/trabalho-1/FileSearch.hpp
+++ materias/obrigatorias/organizacao-estrutura-de-arquivos/trabalho-1/FileSearch.hpp
@@ -1,4 +1,5 @@
 #include "FileSearch.cpp"
+#include <string>
 
 class FileSearch{
 public:
@@ -6,7 +7,7 @@ public:
     void read_file(std::string path, std::string file_ext);
     std::string get_file(int head, int tail);
     void sort(bool inplace = true);
-    int search(int value);
+    int search(int value, std::string by="cep");
     void get_file(); // deve ser do tipo File, retorna o objeto File o qual podemos pegar o indice e buscar o cpf
 
 };
